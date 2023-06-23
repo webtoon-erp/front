809a6025a3c4e9541097a4a3e4c6a8611eb3ab3e
[jieunyy] <ina011011@naver.com>
2023-06-23 16:11:32 +0900

    [component] footer 컴포넌트 구현

--- src/component/footer.tsx
+++ src/component/footer.tsx
@@ -1,3 +1,15 @@
 const Footer = () => {
+    <>
+        <nav>
+            <a href="" target='_blank'>Blog</a>
+            <a href="" target='_blank'>Github</a>
+        </nav>
+        <p>
+            <span>저자: </span>
+            <span>이메일: </span>
+            <span>Copyright 2022. 저자. All Rights Reserved: </span>
+        </p>
+    </>
 }
+
 export default Footer;
