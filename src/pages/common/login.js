c5ec8995566689bbdfa54b682d2e045a373058c6
[jieunyy] <ina011011@naver.com>
2023-06-28 12:41:50 +0900

    feat: 로그인 화면 구현

--- src/pages/common/login.js
+++ src/pages/common/login.js
@@ -2,12 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 import { Input, Button } from 'antd';
 
+const BoldText = styled.span`
+  font-weight: bold;
+  font-size: 24px;
+  font-family: 'Montserrat', sans-serif;
+  padding: 10px;
+`;
+
 const LoginContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   height: 100vh;
+  padding: 24px;
 `;
 
 const LoginForm = styled.form`
@@ -27,6 +35,12 @@ const LoginButton = styled(Button)`
   margin-top: 16px;
 `;
 
+const CenteredContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const LoginScreen = () => {
   const handleLogin = (e) => {
     e.preventDefault();
@@ -34,20 +48,25 @@ const LoginScreen = () => {
   };
 
   return (
-    <LoginContainer>
-      <LoginForm onSubmit={handleLogin}>
-        <InputWrapper>
-          <Input type="text" placeholder="아이디" />
-        </InputWrapper>
-        <InputWrapper>
-          <Input type="password" placeholder="비밀번호" />
-        </InputWrapper>
-        <LoginButton type="primary" htmlType="submit">
-          로그인
-        </LoginButton>
-      </LoginForm>
-    </LoginContainer>
+    <CenteredContainer>
+      <LoginContainer>
+        <BoldText>웹툰 erp</BoldText>
+        <LoginForm onSubmit={handleLogin}>
+          <InputWrapper>
+            <Input type="text" placeholder="아이디" />
+          </InputWrapper>
+          <InputWrapper>
+            <Input type="password" placeholder="비밀번호" />
+          </InputWrapper>
+          <LoginButton type="primary" htmlType="submit">
+            로그인
+          </LoginButton>
+        </LoginForm>
+      </LoginContainer>
+    </CenteredContainer>
   );
 };
 
 export default LoginScreen;
+
+
