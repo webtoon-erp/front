5173799080e9ca24d5596e365e9a5f383af05f99
SO HUI <haejuuun925@gmail.com>
2023-07-07 22:43:22 +0900

    feat: 직원관리 화면 구현

--- src/pages/hr/hrView.js
+++ src/pages/hr/hrView.js
@@ -1,7 +1,16 @@
+import { styled } from "styled-components";
+import EmployeeView from "../../component/forHr/forHrView/employeeView";
+
+const EmpProfileContainer = styled.div`
+    margin-top: 100px;
+    margin-left: 10px;
+    width: 100%;
+`;
+
 export default function HrView (){
     return(
-        <>
-        <div>HrView</div>
-        </>
+        <EmpProfileContainer>
+            <EmployeeView />
+        </EmpProfileContainer>
     )
 }
