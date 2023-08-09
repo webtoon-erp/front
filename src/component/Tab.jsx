bd05494ad76009a9652e64320dc64a7588e763cc
[jieunyy] <ina011011@naver.com>
2023-08-09 21:40:03 +0900

    feat: 홈 고정 기능 구현

--- src/component/Tab.jsx
+++ src/component/Tab.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Tab = ({ tabElements, onClose, onAdd }) => {
+const Tab = ({ tabElements, onClose }) => {
+  const handleCloseTab = (index) => {
+    if (tabElements[index].fixed) {
+      return;
+    }
+
+    const newTabElements = [...tabElements];
+    newTabElements.splice(index, 1);
+    localStorage.setItem('tabs', JSON.stringify(newTabElements));
+
+    if (tabElements[index].title === localStorage.getItem('activeTab')) {
+      const newActiveTab = newTabElements[newTabElements.length - 1]?.title || null;
+      localStorage.setItem('activeTab', newActiveTab);
+    }
+
+    onClose(index);
+  };
+
   return (
-    <div className="tab-container" style={{display: 'flex', flexDirection: 'row'}}>
+    <div className="tab-container" style={{ display: 'flex', flexDirection: 'row' }}>
       {tabElements.map((tab, index) => (
-        <div key={index} className={`tab ${tab.fixed ? 'fixed' : ''}`} style={{border: '1px solid #ccc', padding: '2px'}}>
-          {tab.title}
-          {!tab.fixed && onClose && (
-            <button onClick={() => onClose(index)} className="close-button">
+        <div
+          key={index}
+          className={`tab ${tab.fixed ? 'fixed' : ''}`}
+          style={{ border: '1px solid #ccc', padding: '2px' }}
+        >
+          <Link to={ tab.title === 'Home'? `/` : `/${tab.title.toLowerCase()}`} className="tab-link">
+            {tab.title}
+          </Link>
+          {!tab.fixed && (
+            <button onClick={() => handleCloseTab(index)} className="close-button">
               X
             </button>
           )}
@@ -18,3 +42,5 @@ const Tab = ({ tabElements, onClose, onAdd }) => {
 };
 
 export default Tab;
+
+
