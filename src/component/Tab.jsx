2b2130fb28508ed4b9b19a9f0cfe500f95dd9eb2
SO HUI <haejuuun925@gmail.com>
2023-08-12 02:18:40 +0900

    style: tab style update

--- src/component/Tab.jsx
+++ src/component/Tab.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import theme from '../style/theme';
 
 const Tab = ({ tabElements, onClose }) => {
   const handleCloseTab = (index) => {
@@ -20,20 +22,18 @@ const Tab = ({ tabElements, onClose }) => {
   };
 
   return (
-    <div className="tab-container" style={{ display: 'flex', flexDirection: 'row' }}>
+    <div className="tab-container" style={{ display: 'flex', flexDirection: 'row'}}>
       {tabElements.map((tab, index) => (
         <div
           key={index}
           className={`tab ${tab.fixed ? 'fixed' : ''}`}
-          style={{ border: '1px solid #ccc', padding: '2px' }}
+          style={{ border: '1px solid #ccc', padding: '5px' }}
         >
-          <Link to={ tab.title === 'Home'? `/` : `/${tab.title.toLowerCase()}`} className="tab-link">
+          <Link to={ tab.title === 'Home'? `/` : `/${tab.title.toLowerCase()}`} className="tab-link" style={{ textDecoration: "none" }}>
             {tab.title}
           </Link>
           {!tab.fixed && (
-            <button onClick={() => handleCloseTab(index)} className="close-button">
-              X
-            </button>
+            <DeleteBtn onClick={() => handleCloseTab(index)} className="close-button" />
           )}
         </div>
       ))}
@@ -43,4 +43,19 @@ const Tab = ({ tabElements, onClose }) => {
 
 export default Tab;
 
+const DeleteBtn = styled.button`
+  border: none;
+  width: 16.5px;
+  height: 16.5px;
+  border-radius: 50%;
+  padding: 1px 1px 0px 0px;
+  margin-left: 5px;
+  background-color: #6FC4FD;
+  cursor: pointer;
+  color: ${theme.colors.ff};
+  &:after {
+    content: "\\00d7";
+    font-size: 15.3px;
+  }
+`;
 
