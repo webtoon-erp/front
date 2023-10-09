16dad0503c36407ff04655f3503843f859be4f23
[jieunyy] <ina011011@naver.com>
2023-10-09 14:35:19 +0900

    feat: 서비스요청, 코멘트 기능 보완

--- src/component/comments/commentComponent.jsx
+++ src/component/comments/commentComponent.jsx
@@ -1,29 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import CommentForm from './commentForm';
-import CommentList from './commentList';
-import { message } from 'antd';
+import theme from '../../style/theme';
 import axios from 'axios';
 
 const CommentComponent = ({ webtoonDtId }) => {
-  const [list, setList] = useState([]);
   const [userId, setUserId] = useState('');
+  const [value, setValue] = useState('');
 
   useEffect(() => {
     setUserId(sessionStorage.getItem("employeeId"));
   }, []);
 
-  const addList = (newComment) => {
-    setList([...list, newComment]);
+  const addComment = (newComment) => {
 
     const data = {
-      msgType: 'feedback',
-      content: newComment.content,
+      msgType: 'webtoon',
+      content: newComment,
       refId: webtoonDtId,
       programId: 'WT',
       sendEmpId: userId,
     };
-    console.log("data", data)
+    console.log("data", data);
 
     axios
       .post('http://146.56.98.153:8080/webtoonDt/feedBack', data, {
@@ -32,28 +29,66 @@ const CommentComponent = ({ webtoonDtId }) => {
         },
       })
       .then((response) => {
-        message.success('API Response: ' + JSON.stringify(response.data));
+        console.log('API Response:', response.data);
       })
       .catch((error) => {
-        message.error('API Error: ' + error.message);
+        console.error('API Error:', error.message);
       });
+  };
+
+  const formatDate = (date) => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
   };
 
   return (
     <CommentContainer>
-      <Container>
-        <CommentForm addList={addList} />
-      </Container>
-      
-      <Container2>
-        <CommentList list={list} />
-      </Container2>
+      <Form onSubmit={(e) => e.preventDefault()}>
+        <span>
+          <Input
+            type='text'
+            placeholder='댓글을 입력해주세요.'
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+        </span>
+        <Btn onClick={() => addComment(value)}>
+          등록
+        </Btn>
+      </Form>
     </CommentContainer>
-      
   );
 };
 
 export default CommentComponent;
+
+const Form = styled.form`
+  width:  600px;
+`;
+
+const Input = styled.input`
+  width: 500px;
+  height: 40px;
+`;
+
+const Btn = styled.button`
+    width: 90px;
+    height: 40px;
+    background-color: ${theme.colors.btn};
+    border: none;
+    color: ${theme.colors.white};
+    text-align: center;
+    border-radius: 8px;
+    box-shadow: 0 5px 10px rgba(0,0,0,0.10), 0 2px 2px rgba(0,0,0,0.20);
+    &:hover {
+        background-color: #00B757;
+    }
+    cursor: pointer;
+    margin: 0px 15px 0px 20px;
+    float: right;
+`
 
 const Container = styled.div`
     display: flex;
