f5e5e28d7c4d7b1a804ab897daa38b4cb1833874
[jieunyy] <ina011011@naver.com>
2023-10-12 19:08:09 +0900

    feat: 회차 상세 댓글 기능 및 홈 캘린더 구현

--- src/component/comments/commentComponent.jsx
+++ src/component/comments/commentComponent.jsx
@@ -2,25 +2,50 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import theme from '../../style/theme';
 import axios from 'axios';
+import { message } from 'antd';
+import CommentList from './commentList';
 
 const CommentComponent = ({ webtoonDtId }) => {
   const [userId, setUserId] = useState('');
+  const [list, setList] = useState(['피드백내용', '발신자','사번']);
   const [value, setValue] = useState('');
+  const [toonId, setToonId] = useState('');
+  const [trigger, setTrigger] = useState(0);
 
   useEffect(() => {
+    const data = {
+      webtoonDtID : webtoonDtId,
+    };
+
+    axios
+      .get('http://146.56.98.153:8080/webtoonDt/'+webtoonDtId, {
+        data: data,  
+        headers: {
+          'Content-Type': 'application/json;charset=UTF-8',
+        },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          setList(response.data.info.feedbackList);
+        }
+      })
+      .catch((error) => {
+        message.error('데이터를 불러오는데 실패했습니다.', error);
+      });
+  }, [webtoonDtId, trigger]);
+
+  useEffect(() => {
+    setToonId(localStorage.getItem('Id'));
     setUserId(sessionStorage.getItem("employeeId"));
   }, []);
 
   const addComment = (newComment) => {
-
     const data = {
-      msgType: 'webtoon',
       content: newComment,
       refId: webtoonDtId,
       programId: 'WT',
       sendEmpId: userId,
     };
-    console.log("data", data);
 
     axios
       .post('http://146.56.98.153:8080/webtoonDt/feedBack', data, {
@@ -29,7 +54,13 @@ const CommentComponent = ({ webtoonDtId }) => {
         },
       })
       .then((response) => {
-        console.log('API Response:', response.data);
+        if (response.status === 200) {
+          // Update the list state with the new comment
+          message.success('피드백 등록 완료.');
+          setTrigger(trigger + 1);
+        } else {
+          message.error('작품이 정상적으로 등록되지 않았습니다.');
+        }
       })
       .catch((error) => {
         console.error('API Error:', error.message);
@@ -46,18 +77,17 @@ const CommentComponent = ({ webtoonDtId }) => {
   return (
     <CommentContainer>
       <Form onSubmit={(e) => e.preventDefault()}>
-        <span>
-          <Input
-            type='text'
-            placeholder='댓글을 입력해주세요.'
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-          />
-        </span>
+        <Input
+          type='text'
+          placeholder='댓글을 입력해주세요.'
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
         <Btn onClick={() => addComment(value)}>
           등록
         </Btn>
       </Form>
+      <CommentList list={list}/>
     </CommentContainer>
   );
 };
@@ -65,45 +95,38 @@ const CommentComponent = ({ webtoonDtId }) => {
 export default CommentComponent;
 
 const Form = styled.form`
-  width:  600px;
+  width: 600px;
+  display: flex; /* Add this to make the Input and Btn side by side */
+  align-items: center; /* Optional, align vertically in the middle */
 `;
 
 const Input = styled.input`
   width: 500px;
   height: 40px;
+  flex: 1; /* Allow the input to take available space */
 `;
 
 const Btn = styled.button`
-    width: 90px;
-    height: 40px;
-    background-color: ${theme.colors.btn};
-    border: none;
-    color: ${theme.colors.white};
-    text-align: center;
-    border-radius: 8px;
-    box-shadow: 0 5px 10px rgba(0,0,0,0.10), 0 2px 2px rgba(0,0,0,0.20);
-    &:hover {
-        background-color: #00B757;
-    }
-    cursor: pointer;
-    margin: 0px 15px 0px 20px;
-    float: right;
-`
-
-const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    padding: 20px;
-    padding-left: 15%;
-`;
-
-const Container2 = styled.div`
-    padding-left: 17%;
+  width: 90px;
+  height: 40px;
+  background-color: ${theme.colors.btn};
+  border: none;
+  color: ${theme.colors.white};
+  text-align: center;
+  border-radius: 8px;
+  box-shadow: 0 5px 10px rgba(0,0,0,0.10), 0 2px 2px rgba(0,0,0,0.20);
+  &:hover {
+    background-color: #00B757;
+  }
+  cursor: pointer;
+  margin-left: 20px;
+  flex: none; /* Don't allow it to grow */
+  float: right;
 `;
 
 const CommentContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    padding: 20px;
-    padding-left: 15%;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  padding-left: 15%;
 `;
