ea255ce9e4619da25a43456274ee4c3a38e2035b
SO HUI <haejuuun925@gmail.com>
2023-10-21 02:21:01 +0900

    feat: ag-grid-community 최신 버전으로 업데이트 반영

--- src/component/forElectropayment/myDocsView.jsx
+++ src/component/forElectropayment/myDocsView.jsx
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios'; 
 import styled from 'styled-components';
 import { AgGridReact } from 'ag-grid-react';
-import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-alpine.css';
+import '@ag-grid-community/all-modules/dist/styles/ag-grid.css';
+import '@ag-grid-community/all-modules/dist/styles/ag-theme-alpine.css';
 
 const MyDocsView = () => {
     const [selectedCell, setSelectedCell] = useState(null);
