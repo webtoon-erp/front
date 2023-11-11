e1f33e3c8869d0a4b5f8f292253292e98807d6fc
[jieunyy] <ina011011@naver.com>
2023-11-11 18:22:32 +0900

    fix: 근태관리, 비밀번호 초기화, 페이지 네비게이션, 일정 조회 삭제 기능 오류 수정, 헤더 수정, 화면 이동 구현

--- src/pages/electropayment/epFormAnnualLeaveRequest.js
+++
@@ -1,7 +0,0 @@
-import AnnualLeaveRequestView from "../../component/forElectropaymentRuqeust/forEpForm/annualLeaveRequestView.jsx";
-
-export default function EpFormAnnualLeaveRequest (){
-    return(
-        <AnnualLeaveRequestView />
-    )
-}
