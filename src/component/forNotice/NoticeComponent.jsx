176bb9687e2504a375a0caa4aa6e655b3914523c
SO HUI <haejuuun925@gmail.com>
2023-11-11 21:06:11 +0900

    feat: 공지사항 셀 클릭시 상세페이지로 이동

--- src/component/forNotice/NoticeComponent.jsx
+++ src/component/forNotice/NoticeComponent.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback, useRef } from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import styled from 'styled-components';
 import { AgGridReact } from 'ag-grid-react';
@@ -8,15 +8,16 @@ import 'ag-grid-community/styles/ag-theme-alpine.css';
 
 const NoticeComponent = () => {
     const [selectedTag, setSelectedTag] = useState('전체');
-    const [rowData, setRowData] = useState([]);  
+    const [rowData, setRowData] = useState([]);
+    const gridRef = useRef(null);
   
   useEffect(() => {
     // Make a GET request to fetch data
     axios.get('http://146.56.98.153:8080/notice', {
-      headers: {
-        'Content-Type': 'application/json;charset=UTF-8',
-      },
-    })
+        headers: {
+          'Content-Type': 'application/json;charset=UTF-8',
+        },
+      })
     .then((response) => {
       if (response.status === 200) {
         setRowData(response.data);
@@ -31,69 +32,43 @@ const NoticeComponent = () => {
     const selectTagHandler = (e) => {
       setSelectedTag(e.target.value);
     };
+
+    const onGridReady = (params) => {
+      gridRef.current = params.api;
+    };
+
+    const navigate = useNavigate();
+
+    const handleNoticeRowClick = (event) => {
+      if (event.data.id) {
+        navigate(`/noticeDetail/${event.data.id}`);
+      }
+    };
   
     const filteredNotices = rowData.filter((notice) => selectedTag === '전체' ? true : notice.noticeType === selectedTag);
   
     // ag-grid
     const columnDefs = [
       { headerName: '태그', field: 'noticeType', width: 100 },
-      { headerName: '제목', field: 'title', width: 400, cellRendererFramework: CellRenderer },
+      { headerName: '제목', field: 'title', width: 400 },
       { headerName: '작성자', field: 'name', width: 100 },
       { headerName: '부서', field: 'deptName', width: 100 },
       { headerName: '작성일자', field: 'noticeDate', width: 200 },
       { headerName: '조회수', field: 'readCount', width: 100 },
     ];
-  
-    const gridOptions = {
-      columnDefs: columnDefs,
-      rowData: filteredNotices,
-      rowSelection: 'single',
-      animateRows: true,
-      pagination: true,
-      paginationPageSize: 10,
-      onCellClicked: handleCellClick,
-    };
-  
-    // 셀 클릭 핸들러
-    function handleCellClick(event) {
-      const column = event.colDef.field;
-      const rowData = event.data;
-      const url = rowData.url;
-      if (column  && url) {
-        window.location.href = url;
-      }
-    }
-  
-    // 사용자 정의 셀 렌더러
-    function CellRenderer(props) {
-      const rowData = props.data;
-      const title = rowData.title;
-      const url = rowData.url;
-      return (
-        <Link to={url}>{title}</Link>
-      );
-    }
-
-    //검색
-    const gridRef = useRef(null);
 
       useEffect(() => {
-        gridRef.current = gridOptions.api;
+        // 컴포넌트가 마운트될 때 초기 필터 설정
+        if (gridRef.current) {
+          gridRef.current.setQuickFilter(document.getElementById('filter-text-box').value);
+        }
       }, []);
 
       const onFilterTextBoxChanged = useCallback(() => {
-        gridRef.current.setQuickFilter(
-          document.getElementById('filter-text-box').value
-        );
-      }, []);
-      
-
-      const onPrintQuickFilterTexts = useCallback(() => {
-        gridRef.current.forEachNode((rowNode, index) => {
-          console.log(
-            'Row ' + index + ' quick filter text is ' + rowNode.quickFilterAggregateText
-          );
-        });
+        // 필터 텍스트가 변경될 때 필터 설정
+        if (gridRef.current) {
+          gridRef.current.setQuickFilter(document.getElementById('filter-text-box').value);
+        }
       }, []);
   
     return (
@@ -121,7 +96,12 @@ const NoticeComponent = () => {
           <AgGridReact
             columnDefs={columnDefs}
             rowData={filteredNotices}
-            gridOptions={gridOptions}
+            rowSelection= 'single'
+            animateRows= {true}
+            pagination= {true}
+            paginationPageSize= {10}
+            onGridReady= {onGridReady}
+            onCellClicked={handleNoticeRowClick}
           />
         </div>
         </NoticeContainer>
