e96c93c4936dec6b968fc562e9803f332dbf4775
SO HUI <haejuuun925@gmail.com>
2023-11-25 17:22:52 +0900

    feat: 서비스 조회 update

--- src/component/forITSM/forServiceView/viewList.jsx
+++ src/component/forITSM/forServiceView/viewList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/styles/ag-grid.css';
@@ -6,26 +6,22 @@ import 'ag-grid-community/styles/ag-theme-alpine.css';
 import { message } from 'antd';
 import axios from 'axios';
 import styled from 'styled-components';
+import Chart from 'chart.js/auto';
+import 'chartjs-plugin-datalabels';
+import HorizonLine from '../../horizonLine';
+import theme from '../../../style/theme';
 
 const ViewList = () => {
     const [rowData, setRowData] = useState([]);
 
-    // const rowData = [
-    //     {제목: 'title1', 요청자: 'employee1', 납기일: '2023-06-15', 승인자: 'employee3', 상태: '완료', url: '/title1'},
-    //     {제목: 'title2', 요청자: 'employee2', 납기일: '2023-06-30', 승인자: 'employee3', 상태: '완료', url: '/title2'},
-    //     {제목: 'title3', 요청자: 'employee2', 납기일: '2023-06-30', 승인자: 'employee4', 상태: '완료', url: '/title3'},
-    //     {제목: 'title4', 요청자: 'employee3', 납기일: '2023-07-03', 승인자: 'employee2', 상태: '진행', url: '/title4'},
-    //     {제목: 'title5', 요청자: 'employee4', 납기일: '2023-07-14', 승인자: '', 상태: '요청', url: '/title5'},
-    //     {제목: 'title6', 요청자: 'employee2', 납기일: '2023-07-14', 승인자: '', 상태: '반려', url: '/title6'},
-    // ];
-
     const columnDefs = [
-        {field: '제목', sortable: true, filter: true, width: '150px'},
-        {field: '요청자', sortable: true, filter: true, width: '130px'},
-        {field: '납기일', sortable: true, filter: true, width: '130px'},
-        {field: '승인자', sortable: true, filter: true, width: '130px'},
+        {headerName: '제목', field: 'title', sortable: true, filter: true, width: '150px'},
+        {headerName: '요청자', field: 'reqUser', sortable: true, filter: true, width: '130px'},
+        {headerName: '납기일', field: 'LocalDate', sortable: true, filter: true, width: '130px'},
+        {headerName: '승인자', field: 'itUser', sortable: true, filter: true, width: '130px'},
         {
-            field: '상태',
+            headerName: '상태',
+            field: 'step',
             sortable: true,
             filter: true,
             width: '80px',
@@ -41,18 +37,9 @@ const ViewList = () => {
 
     const navigate = useNavigate();
 
-    // function handleCellClick(event) {
-    //     const column = event.colDef.field;
-    //     const requests = event.data;
-    //     const url = requests.url;
-    //     if (column  && url) {
-    //         window.location.href = url;
-    //     }
-    // }
-
     const handleRowClick = (event) => {
         if (event.data.id) {
-            navigate(`/itRequestView/${event.data.id}`);
+            navigate(`/itRequesDetail/${event.data.id}`);
             console.log('확인');
         }
     };
@@ -61,7 +48,7 @@ const ViewList = () => {
         const userId = sessionStorage.getItem('employeeId');
 
         if(userId) {
-            axios.get(`http://146.56.98.153:8080/request/all/${userId}`, {
+            axios.get(`http://146.56.98.153:8080/request/list/${userId}`, {
                 headers: {
                     'Content-Type': 'application/json;charset=UTF-8',
                 },
@@ -69,7 +56,7 @@ const ViewList = () => {
             .then((response) => {
                 if (response.status === 200) {
                     setRowData(response.data);
-                    console.log("response.data", response.data);
+                    console.log("요청 성공");
                 } else {
                     message.error('데이터를 불러오는데 실패했습니다.');
                 }
@@ -79,23 +66,104 @@ const ViewList = () => {
             });
         }
     }, []);
+
+    const chartRef = useRef(null);
+
+    useEffect(() => {
+
+        const chartCanvas = chartRef.current;
+        const chartData = [2, 3, 1];
+    
+        const chart = new Chart(chartCanvas, {
+            type: 'doughnut',
+            data: {
+                labels: ['미완료', '완료', '진행'],
+                datasets: [
+                    {
+                        label: '개수',
+                        data: chartData,
+                        backgroundColor: ['rgba(255, 99, 132, 0.5)', 'rgba(54, 162, 235, 0.5)', 'rgba(145,242,155,0.7)'],
+                        borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(145,242,155,1)'],
+                        borderWidth: 1,
+                    },
+                ],
+            },
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: {
+                        display: true,
+                        position: 'bottom',
+                    },
+                    datalabels: {
+                        formatter: (value, context) => {
+                            const dataset = context.chart.data.datasets[0];
+                            const total = dataset.data.reduce((acc, cur) => acc + cur, 0);
+                            const percentage = ((value / total) * 100).toFixed(1);
+                            return `${value}개 (${percentage}%)`;
+                        },
+                    },
+                },
+                animation: {
+                    animateScale: true,
+                    animateRotate: true,
+                },
+                tooltips: {
+                    enabled: false,
+                },
+            },
+        });
+    
+        return () => {
+            chart.destroy();
+        };
+    }, []);
     
     return(
-        <ViewListContainer>
-            <RequestListGrid className="ag-theme-alpine">
-                <AgGridReact 
-                    rowData={rowData}
-                    columnDefs={columnDefs}
-                    animateRows={true}
-                    rowSelection='single'
-                    domLayout= 'autoHeight'
-                    pagination= {true}
-                    paginationPageSize= {5}
-                    onCellClicked= {handleRowClick}
+        <FlexBox>
+            <ViewListContainer>
+                <RequestListGrid className="ag-theme-alpine">
+                    <AgGridReact 
+                        rowData={rowData}
+                        columnDefs={columnDefs}
+                        animateRows={true}
+                        rowSelection='single'
+                        domLayout= 'autoHeight'
+                        pagination= {true}
+                        paginationPageSize= {5}
+                        onCellClicked= {handleRowClick}
+                    />
+                </RequestListGrid>
+            </ViewListContainer>
 
-                />
-            </RequestListGrid>
-        </ViewListContainer>
+            <RequestStateContainer>
+                <Title>요청 상태 현황</Title>
+                <HorizonLine />
+                <FlexBox>
+                    <MoreOuterBox>
+                        <OuterBox>
+                            <Box>
+                                <h3 style={{ 'margin-top': '0' }}>완료</h3>
+                                <Count style={{ 'background-color': '#91CDF2' }}>3</Count>
+                            </Box>
+                            <Box>
+                                <h3 style={{ 'margin-top': '0' }}>미완료</h3>
+                                <Count style={{ 'background-color': '#F2ACBF' }}>2</Count>
+                            </Box>
+                        </OuterBox>
+                        <Box>
+                            <h3 style={{ 'margin-top': '0' }}>진행</h3>
+                            <Count style={{ 'background-color': '#91F29B' }}>1</Count>
+                        </Box>
+                    </MoreOuterBox>
+                    
+
+                    <RequestStateGrid className="ag-theme-alpine" style={{ height: '200px', width: '200px' }}>
+                        <canvas ref={chartRef} width="200px" height="200px" />
+                    </RequestStateGrid>
+                </FlexBox>
+            </RequestStateContainer>
+        </FlexBox>
     );
 };
 export default ViewList;
@@ -104,21 +172,61 @@ const ViewListContainer = styled.div`
     padding: 10px;
     border: 1px solid #ccc;
     border-radius: 8px;
+    margin-right: 100px;
 `;
 
 const RequestListGrid = styled.div`
-    width: auto;
+    width: 500px;
     height: auto;
     overflow: hidden;
-`
+`;
 
-// 추후 ag-grid에 적용 가능할 시 사용
-// const StateBox = styled.div`
-//     width: 20px;
-//     height: 15px;
-//     border-radius: 50px;
-//     background-color: #91CDF2;
-//     color: ${theme.colors.white};
-//     box-shadow: 0 5px 10px rgba(0,0,0,0.10), 0 2px 2px rgba(0,0,0,0.20);
-//     text-align: center;
-// `
+const RequestStateContainer = styled.div`
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 8px;
+    height: 350px;
+    width: 490px;
+`;
+
+const FlexBox = styled.div`
+    display : flex;
+`;
+
+const Title = styled.h3`
+    margin-left: 10px;
+`;
+
+const RequestStateGrid = styled.div`
+    width: 400px;
+    height: 110px;
+    margin-top: 30px;
+    margin-left: 10px;
+`;
+
+const MoreOuterBox = styled.div`
+`;
+
+const OuterBox = styled.div`
+    display : flex;
+    margin-bottom: 25px;
+`;
+
+const Box = styled.div`
+    text-align: center;
+    margin-left: 20px;
+    margin-right: 20px;
+`;
+
+const Count = styled.div`
+    display : flex;
+    justify-content : center;
+    align-items : center;
+    font-size: 35px;
+    color: ${theme.colors.white};
+    width: 100px;
+    height: 70px;
+    border-radius: 8px;
+    margin: auto;
+    box-shadow: 0 5px 10px rgba(0,0.10,0,0.10), 0 2px 2px rgba(0,0.10,0,0.10);
+`;
--- src/pages/itsm/itRequestListView.js
+++ src/pages/itsm/itRequestListView.js
@@ -1,19 +1,14 @@
 import { styled } from "styled-components";
-import ServiceState from "../../component/forITSM/forServiceView/serviceState";
 import ViewList from "../../component/forITSM/forServiceView/viewList";
 
 export default function ItRequestListView (){
     return(
         <>
             <Title>서비스 조회</Title>
-            <FlexBox>
                 <ViewListContainer>
                     <ViewList />
                 </ViewListContainer>
-                <ServiceStateContainer>
-                    <ServiceState />
-                </ServiceStateContainer>
-            </FlexBox>
+                
             
         </>
     )
@@ -25,18 +20,6 @@ const Title = styled.div`
     padding-left: 2%;
     font-weight: bold;
 `;
-
-const FlexBox = styled.div`
-    display : flex;
-    justify-content : center;
-    align-items : center;
-`
-
-const ServiceStateContainer = styled.div`
-    padding-left: 2%;
-    margin-top: 30px; 
-    width: 40%
-`
 
 const ViewListContainer = styled.div`
     padding-left: 2%;
